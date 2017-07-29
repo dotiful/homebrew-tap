b2d992f955300e3307f5626491f201c1cfc5b427
dotiful <artdevjs@gmail.com>
2017-07-29 21:08:29 +0300

    Add Keep Drive Spinning.app v2.6.2

---
+++ Casks/keep-drive-spinning.rb
@@ -0,0 +1,10 @@
+cask 'keep-drive-spinning' do
+  version '2.6.2'
+  sha256 'e68ae84a71841bc134290a0fa863ff5c7a78b4f725ecc0ffc8eec97a6444ec4d'
+
+  url 'http://jon.stovell.info/software/download.php?d=KeepDriveSpinning.zip'
+  name 'Keep Drive Spinning'
+  homepage 'http://jon.stovell.info/software/keep-drive-spinning/'
+
+  app 'Keep Drive Spinning.app'
+end
