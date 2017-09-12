a8f4078eecc43c6744ed3c5b3c5c74983fe12431
dotiful <artdevjs@gmail.com>
2017-09-12 14:50:46 +0300

    Update themer-gui to 0.4.0

--- Casks/themer-gui.rb
+++ Casks/themer-gui.rb
@@ -1,10 +1,10 @@
-cask 'themer' do
-  version '0.3.0'
-  sha256 '11d90257ea05427b9d57f9fbf3bb741ea46f4d3c95fd0598440314d870d05cdc'
+cask 'themer-gui' do
+  version '0.4.0'
+  sha256 'e8c8a4607782c116f5321e73be4498509d8d8232da708a0e5b8fac087ae62549'
 
-  url 'https://github.com/mjswensen/themer-gui/releases/download/v0.3.0/Themer-0.3.0.dmg'
+  url "https://github.com/mjswensen/themer-gui/releases/download/v#{version}/Themer-#{version}.dmg"
   appcast 'https://github.com/mjswensen/themer-gui/releases.atom',
-          checkpoint: '8c52f25f0cca3f3a2cec2765f0193339961a18cd380529fca6d854a53a18fb7d'
+          checkpoint: 'e5b451b7a63ee7ed0e9eacb6a89bfec864950f570a41e4ea2724dfb2a6f9f8a6'
   name 'SSHFS GUI'
   homepage 'https://github.com/mjswensen/themer-gui'
 
