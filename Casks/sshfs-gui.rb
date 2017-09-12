8f5e8c73cb776ae8151e624eb63953c361123b91
dotiful <artdevjs@gmail.com>
2017-09-12 14:50:08 +0300

    Fixed url in sshfs-gui cask

--- Casks/sshfs-gui.rb
+++ Casks/sshfs-gui.rb
@@ -2,7 +2,7 @@ cask 'sshfs-gui' do
   version '1.3.1'
   sha256 '45475db428e2bfa30788996ebe330fa9ac02e6cb945dcdb564b4497218efc246'
 
-  url 'https://github.com/dstuecken/sshfs-gui/raw/develop/Installer/sshfs-gui-1.3.1.dmg'
+  url "https://github.com/dstuecken/sshfs-gui/raw/develop/Installer/sshfs-gui-#{version}.dmg"
   appcast 'https://github.com/dstuecken/sshfs-gui/releases.atom',
           checkpoint: '2431e3a3aa6f701201f1728f4b30e01eef068febebb3353022fc6fafda8ee101'
   name 'SSHFS GUI'
