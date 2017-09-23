55f97a90779c239e2b40cff3a2dc3c42469bc351
dotiful <artdevjs@gmail.com>
2017-09-23 22:53:45 +0300

    Update themer-gui to 0.7.0

--- Casks/themer-gui.rb
+++ Casks/themer-gui.rb
@@ -1,10 +1,10 @@
 cask 'themer-gui' do
-  version '0.6.1'
-  sha256 '5f7a457674bef65db3b6a17558c16aff8a56c9dc5a264435d2b34ebd98209a0e'
+  version '0.7.0'
+  sha256 '13eb649e5030a10c004f1a058eba9b3ab03d8ffe35a7e177085597d3b4248ef6'
 
   url "https://github.com/mjswensen/themer-gui/releases/download/v#{version}/Themer-#{version}.dmg"
   appcast 'https://github.com/mjswensen/themer-gui/releases.atom',
-          checkpoint: '4e99881eab877e3036abc9cf9c01c6513490a2692f3e83c6835314e2b98cbffd'
+          checkpoint: '8c66b4d5a12cd1f82da4ad370444fc76619d8f2980f453ae503b14a73e552c60'
   name 'Themer'
   homepage 'https://github.com/mjswensen/themer-gui'
 
